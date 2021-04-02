44917aaa2af2f89a81c5404b51de7ae36f0137a1
John Moss <john.moss@ranplanwireless.com>
2021-04-02 11:56:43 +0100

    Initially commit of console app project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JwtTokenGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
